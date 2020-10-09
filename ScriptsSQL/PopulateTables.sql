d52710c4178eb0c2b0ffb34073de74a20237e4da
angipaola10 <angi.jimenez@mail.escuelaing.edu.co>
2020-10-09 16:32:03 -0500

    Arreglando post evento

--- ScriptsSQL/PopulateTables.sql
+++ ScriptsSQL/PopulateTables.sql
@@ -1,10 +1,10 @@
 insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('angipaola10', 'd17d5167d65acb3410bf25c3bd12826a','Person', 'angi9236@gmail.com', 3186759533, 'Sin info', '2000-03-10');
 insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('EstebanK23', '078f98aa13719998111a79642a11b364','Person', 'esteban@gmail.com', 3214567654, 'Sin info', '1999-05-11');
 insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('JuanManuelHerreraMoya', '291a4d27fded7a7fc1b0d71e7a9593e3', 'Person','juanma@gmail.com', 3115678909, 'Sin info', '1999-08-12');
-insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('Perritos', 'd17d5167d65acb3410bf25c3bd12826a', 'Company','perritos@gmail.com', 5667890, 'Sin info', '1960-08-12');
-insert into companyevent (eventname, isDonaton, address, eventdate, eventhour, information, hostcompany) values ('Jornada de adopción', false, 'cll 55a No343c - 5', '2020-12-18', '13:00', 'sin info', 4);
+insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('Perritos', 'bccb80ce5063da629c076121e7d3fb8a', 'Refuge','perritos@gmail.com', 5667890, 'Sin info', '1960-08-12');
+insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('Gatitos', 'e7e275e3bee77c9159a81f0a0ec655fd', 'Veterinary','gatitos@gmail.com', 5667896, 'Sin info', '1965-06-18');
+insert into companyevent (eventname, isDonaton, address, eventdate, eventhour, information, hostcompany) values ('Jornada de adopción', false, 'cll 55a No343c - 5', '2020-12-18', '13:00', 'sin info', 5);
 insert into companyevent (eventname, isDonaton, address, eventdate, eventhour, information, hostcompany) values ('Jornada de vacunación', true, 'cll 55a No343c - 5', '2020-12-16', '13:00', 'sin info', 4);
 insert into goal (eventid, prize, state) values (2, 'Kit de aseo para gatos', false);
 insert into goal (eventid, prize, state) values (2, 'Kit de aseo para perros', false);
-insert into participants(iduser,idevent) values (3,2);
 

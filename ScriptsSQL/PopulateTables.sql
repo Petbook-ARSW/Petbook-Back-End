65e021b4df65126d282f3aa5f0535329958b31d4
angipaola10 <angi.jimenez@mail.escuelaing.edu.co>
2020-10-16 19:56:30 -0500

    Arreglando scripts

--- ScriptsSQL/PopulateTables.sql
+++ ScriptsSQL/PopulateTables.sql
@@ -3,8 +3,8 @@ insert into petbookuser (username, pasword, usertype, mail, numberPhone, informa
 insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('JuanManuelHerreraMoya', '291a4d27fded7a7fc1b0d71e7a9593e3', 'Person','juanma@gmail.com', 3115678909, 'Sin info', '1999-08-12');
 insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('Perritos', 'bccb80ce5063da629c076121e7d3fb8a', 'Refuge','perritos@gmail.com', 5667890, 'Sin info', '1960-08-12');
 insert into petbookuser (username, pasword, usertype, mail, numberPhone, information, birthdate) values ('Gatitos', 'e7e275e3bee77c9159a81f0a0ec655fd', 'Veterinary','gatitos@gmail.com', 5667896, 'Sin info', '1965-06-18');
-insert into companyevent (eventname, isDonaton, address, eventdate, eventhour, information, hostcompany) values ('Jornada de adopción', false, 'cll 55a No343c - 5', '2020-12-18', '13:00', 'sin info', 5);
-insert into companyevent (eventname, isDonaton, address, eventdate, eventhour, information, hostcompany) values ('Jornada de vacunación', true, 'cll 55a No343c - 5', '2020-12-16', '13:00', 'sin info', 4);
+insert into companyevent (eventname, isDonaton, address, eventdate, eventhour, information, hostcompany) values ('Jornada de adopcion', false, 'cll 55a No343c - 5', '2020-12-18', '13:00', 'sin info', 5);
+insert into companyevent (eventname, isDonaton, address, eventdate, eventhour, information, hostcompany) values ('Jornada de vacunacion', true, 'cll 55a No343c - 5', '2020-12-16', '13:00', 'sin info', 4);
 insert into goal (eventid, prize, state, valor) values (2, 'Kit de aseo para gatos', false, 250000);
-insert into goal (eventid, prize, state, valor) values (2, 'Kit de aseo para perros', true, 545000);
-insert into pet (idowner,petname,birthdate,information) values (1,'thor','2020-10-10','perro con vacunas contra la rabia');
+insert into goal (eventid, prize, state, valor) values (2, 'Kit de aseo para perros', false, 545000);
+insert into pet (idowner,petname,birthdate,information) values (1,'Valentino','2020-10-10','El perrito mas lindo del mundo');
